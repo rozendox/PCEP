e0657b45a878f5c2b679fd557dc4929a2fe6ffa6
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-01-13 16:32:51 -0300

    Create Perg4.py

---
+++ Perg4.py
@@ -0,0 +1,15 @@
+"""
+Qual será a saída depois de executado o seguinte código? print("Hello","World",sep=None)
+O A. Heilo World
+O B. HelioWorld
+O C. HelioNoneWorld
+O D. Helio,Wor1d
+"""
+
+print("Hello","World",sep=None)
+
+"""
+    output
+    
+    Hello World
+"""  
