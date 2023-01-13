705dc18f0dd88cadc280b4fe3399993c39982e0d
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-01-13 16:48:14 -0300

    Create Perg5.py

---
+++ Perg5.py
@@ -0,0 +1,21 @@
+"""
+Qual será a saída depois de executado o seguinte código? tupll = (-1, O, 1) tup12 = ('bananas') tup13 =
+(tupll, tup12) print(tup13)
+
+A. ((-1, O, 1), 'bananas')
+B. (-1, O, 1, 'bananas')
+C. (-1, O, l)('bananas')
+D. ('bananas', (-1, O, 1))
+
+"""
+
+tupll = (-1, 0, 1)
+tup12 = ('bananas')
+tup13 =(tupll, tup12)
+print(tup13)
+
+
+""""
+  output: 
+    ((-1, 0, 1), 'bananas')
+"""
